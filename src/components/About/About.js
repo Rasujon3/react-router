4708f302651475c781683f2a1fd561feea3f4f7f
Rasujon3 <rasujon3@gmail.com>
2022-03-28 22:42:59 +0600

    52-2 Create Simple Route and simple router setup

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>THis is about us</h1>
+      <p>Wanna know about us?</p>
+      <p>
+        <small>Taile age biriyani khawile...na khauyaile bolmu na.</small>
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>The page u r looking for is not available</h2>
+      <p>404</p>
+    </div>
+  );
+};
+
+export default NotFound;
