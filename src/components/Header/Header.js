777f1b8364434b0a9556247099cbc64eb559dad2
Rasujon3 <rasujon3@gmail.com>
2022-03-29 00:48:02 +0600

    52-5 recap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,12 @@ const Header = () => {
     <div>
       <h1>Welcome to my fancy routing!!!</h1>
       <nav>
-        <CustomLink to="/">Home</CustomLink>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/friends">Friends</Link>
+        {/* <CustomLink to="/">Home</CustomLink>
         <CustomLink to="/friends">Friends</CustomLink>
-        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/about">About</CustomLink> */}
       </nav>
     </div>
   );
